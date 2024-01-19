65dd08447998a858d84e8e8f20aa59f6a6d581e2
mittalbhumika <100149247+mittalbhumika@users.noreply.github.com>
2024-01-19 20:38:06 +0000

    date n time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,11 @@
+// dates
+
+const myDate = new Date()
+// console.log(myDate.toString()); //Fri Jan 19 2024 20:28:54 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toISOString()); //2024-01-19T20:30:00.254Z
+// console.log(myDate.toJSON()); //2024-01-19T20:30:00.254Z
+// console.log(myDate.toLocaleDateString()); //1/19/2024
+// console.log(myDate.toLocaleString()); //1/19/2024, 8:30:00 PM
+
+const myTimeStamp = Date.now()
+console.log(myTimeStamp);
