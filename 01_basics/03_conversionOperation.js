2436d04ef9368a68dd614e3912df31e8253a36b5
mittalbhumika <100149247+mittalbhumika@users.noreply.github.com>
2024-01-19 10:07:28 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = 33;
+
+console.log(typeof score); //number
+
+let age = "22ids"
+console.log(typeof age);
+let convertnumber = Number(age); //string
+console.log(typeof convertnumber); //number
+console.log(convertnumber) //NaN
+
+//"33" => 33
+// "33abc" => NaN (NOT A NUMBER)
+// true/false => 1/0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 33;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+// 1 => true
+// "" => false
+// "hello" => true
+
