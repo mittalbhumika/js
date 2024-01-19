f4b144e2ac4c223a4281bfa75bccf49386155d02
mittalbhumika <100149247+mittalbhumika@users.noreply.github.com>
2024-01-19 11:29:21 +0000

    comparasions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+/*
+
+
+== works differently in js but comparators like < > <=.works differently
+comparators convert null to 0
+there is also === which is aa strict check
+i.e "2" == 2 will give true i.e string apne aap number me convert hogaya
+but "2" === 2 will give false
+
+
+*/
+
