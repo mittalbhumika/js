d9e39da54396f5fe66d35f6c83b90875f7501f8c
mittalbhumika <100149247+mittalbhumika@users.noreply.github.com>
2024-01-19 10:08:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all code as newer version of js
+
+//alert(3+3) // we are using node.js not browser
+
+console.log(typeof "bhumika"); //string
+console.log(typeof 77); //number
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
